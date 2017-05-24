84ddef4353ca4c484b525e33b76c5d33d2083356
AMarkin <chudokreker@ya.ru>
2017-05-24 14:54:25 +0300

    fix gallery and header

--- uncle_project/markup/components/head/head.js
+++ uncle_project/markup/components/head/head.js
@@ -1,11 +1,23 @@
-$('.c-gallery-slider').bxSlider({
-	auto: false,
-	autoControls: true,
-	randomStart: true,
-	pager: false,
-	controls: false
+var headerHeight = $('.c-header').height();
+
+// $('.c-gallery-slider').bxSlider({
+//     auto: false,
+//     autoControls: true,
+//     randomStart: true,
+//     pager: false,
+//     controls: false
+// });
+
+$('.js-magnific-popup').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery:{
+        enabled:true
+    }
 });
 
-$('.js-lightbox-img').magnificPopup({type:'image'});
 
-$('#fullpage').fullpage();
+
+$('#fullpage').fullpage({
+    // paddingTop: headerHeight
+});
